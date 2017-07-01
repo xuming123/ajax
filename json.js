633330b7421ce1e7451d7afcfc8bac0a34bf8467
xurui <xurui@192.168.1.102>
2017-07-01 21:29:03 +0800

    ajax

---
+++ json.js
@@ -0,0 +1,25 @@
+
+{
+	"title":"标题12312312312",
+	"content":"wo是内容",
+
+	"xuexi" : [
+		{
+			"title":"标题12312312",
+			"content":"wo是内容",
+		},
+		{
+			"title":"标题34343",
+			"content":"wo是内容"
+		},
+		{
+			"title":"标题3434",
+			"content":"wo是内容"
+		},
+		{
+			"title":"标题4343",
+			"content":"wo是内容"
+		}
+	]
+}
+
